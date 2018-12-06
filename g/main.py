6f5a9cb53b8698dd08465885a14e82f549f06f66
meawoppl <meawoppl@gmail.com>
2018-12-05 21:45:24 -0800

    Pull out group thing

--- g/main.py
+++ g/main.py
@@ -1,57 +1,7 @@
 import itertools
-from ortools.constraint_solver import pywrapcp
 
 from g.table import SquareTable
-
-
-class Group(SquareTable):
-    I = 1
-
-    def __init__(self, size: int):
-        self._s = pywrapcp.Solver("group_of_size_%i" % size)
-
-        super().__init__(size, initializer=self._initial_table_entry)
-        self._init_constraints()
-
-    def _name(self, var: str, x: int, y: int) -> str:
-        return self.__class__.__name__ + "." + var + "[%i,%i]" % (x, y)
-
-    def _initial_table_entry(self, x, y):
-        if (x == 0):
-            element = self._s.IntConst(y + self.I, self._name("Const", x, y))           
-        elif (y == 0):
-            element = self._s.IntConst(x + self.I, self._name("Const", x, y))
-        else:
-            element = self._s.IntVar(self.I, self._size, self._name("Var", x, y))
-        return element
-
-    def _init_constraints(self):
-        # Entries per row must be unique
-        for x in range(self._size):
-            row = self.get_row(x)
-            self._s.Add(self._s.AllDifferent(row))
-
-        # Entries per col must be unique
-        for y in range(self._size):
-            col = self.get_col(y)
-            self._s.Add(self._s.AllDifferent(col))
-
-        # Ones lead constraint
-        trace_vars = self.get_trace()
-        for first, second in zip(trace_vars[:-1], trace_vars[1:]):
-            both_identity = (first == self.I) and (second == self.I)
-            identity_first = (first == self.I) and (second != self.I)
-            neither_identity = (first != self.I) and (second != self.I)
-            self._s.Add(both_identity or identity_first or neither_identity) 
-
-        for i, trace_var in enumerate(trace_vars[:-1]):
-            trace_element_ident = trace_var == self.I
-            permutation_on_adjacent = (self[i, i+1] == self.I) and (self[i+1,i] == self.I)
-            self._s.Add(trace_element_ident or permutation_on_adjacent)
-
-    def get_solver(self):
-        return self._s
-
+from g.group import Group
 
 def solve_groups(g: Group):
     solver = g.get_solver()
@@ -78,7 +28,6 @@ def solve_groups(g: Group):
 
 def is_isomorphic(g1: Group, g2: Group):
     pass
-
 
 def print_solution(coll, solution_number, group):
     get_xy_value = lambda x, y: to_symbols(coll.Value(solution_number, g[x, y]))
